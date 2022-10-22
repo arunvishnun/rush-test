8d9387f566cf84f7524d9d5a84d0774812254404
Arun Vishnu <arunnampoothiri.vish@walmart.com>
2022-10-21 21:32:54 -0700

    all packages fresh start

---
+++ packages/rush-test-my-controls/src/MyClass.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.MyClass = void 0;
+var MyClass = /** @class */ (function () {
+    function MyClass() {
+    }
+    MyClass.prototype.doSomething = function () {
+        console.log('Hello, world...!');
+    };
+    return MyClass;
+}());
+exports.MyClass = MyClass;
